1000313310d35ae72bbc182944ea3f42628eed87
Alberto Chirinos <developernet@outlook.com>
2020-07-08 14:33:04 -0500

    Se agregó EntityFramework 6 al proyecto. Se agregó cadena de conexión a archivo Web.Config. Se agregó clase ProFinancialMDBContext en carpeta Models.

---
+++ ProFinancialM/ProFinancialM/Models/ProFinancialMDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProFinancialM.Models
+{
+    public class ProFinancialMDBContext: DbContext
+    {
+        public ProFinancialMDBContext()
+            : base("ProFinancialMConnectionString2")
+        {
+
+        }
+    }
+}
